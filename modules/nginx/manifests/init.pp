a95fbb9a3c559bf5dfbee204bd6d9ee8924b4edb
Leonid Mamchenkov <l.mamchenkov@qobo.biz>
2014-10-07 09:23:01 +0300

    More tweaking of php-fpm for Amazon

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -12,13 +12,30 @@ class nginx {
 		Amazon => ['nginx', 'php55-fpm', 'php55-cli', 'php55-xml', 'php55-gd', 'php55-mbstring', 'php55-mcrypt'],
 	}
 
+	$fpm_service = $operatingsystem ? {
+		Fedora => 'php-fpm',
+		CentOS => 'php-fpm',
+		Amazon => 'php-fpm-5.5',
+	}
+
+	$fpm_file_path = $operatingsystem ? {
+		Fedora => '/etc/php-fpm.d/www.conf',
+		CentOS => '/etc/php-fpm.d/www.conf',
+		Amazon => '/etc/php-fpm-5.5.d/www.conf',
+	}
+
 	# Make sure Apache is stopped
 	service { "httpd":
 		ensure => "stopped",
 		enable => "false"
 	}
 
-	service { ['nginx', 'php-fpm']:
+	service { $fpm_service:
+		ensure => "running",
+		enable => "true"
+	}
+
+	service { 'nginx':
 		ensure => "running",
 		enable => "true"
 	}
@@ -51,8 +68,8 @@ class nginx {
 	}
 
 	file { 'www.conf':
-		notify => Service['php-fpm'],
-		path => '/etc/php-fpm.d/www.conf',
+		notify => Service[$fpm_service],
+		path => $fpm_file_path,
 		ensure => file,
 		source => 'puppet:///modules/nginx/www.conf',
 		owner => 'root',

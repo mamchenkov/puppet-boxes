656a75ac5e9e043d2276a0ec59e9d4bb921ac479
Leonid Mamchenkov <l.mamchenkov@qobo.biz>
2014-10-09 16:00:25 +0300

    Renaming manifests for consistency

--- modules/role/manifests/init.pp
+++ modules/role/manifests/init.pp
@@ -1,18 +1,18 @@
 class role {
-	include profile::base
 }
 
 class role::base inherits role{
+	include profile::base
 }
 
-class role::desktop inherits role{
+class role::desktop inherits role::base{
 	include profile::desktop
 }
 
-class role::webserver inherits role{
+class role::webserver inherits role::base{
 	include profile::webserver
 }
 
-class role::dbserver inherits role{
+class role::dbserver inherits role::base{
 	include profile::dbserver
 }

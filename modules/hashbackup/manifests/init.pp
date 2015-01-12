2f6e45fa470cd43f8d4b3d22b01ea9355f5bde70
Leonid Mamchenkov <l.mamchenkov@qobo.biz>
2015-01-12 13:56:32 +0200

    Added cron.daily/hashbackup.sh script

--- modules/hashbackup/manifests/init.pp
+++ modules/hashbackup/manifests/init.pp
@@ -1,5 +1,15 @@
 # Hashbackup - http://www.hashbackup.com/
-class hashbackup {
+class hashbackup (
+	$hashbackup = '/usr/bin/hb',
+	$backup_dir='/var/backup/files',
+	$check_hb_updates='yes',
+	$verbose=0,
+	$dedup_memory=100m,
+	$max_file_size=1g,
+	$retain_policy='-t all',
+	$backup_targets='/etc /root /home /var/www /var/spool/mail /var/log'
+	) {
+
 
 	# Architecture specific binary
 	$binary = $architecture ? {
@@ -7,12 +17,24 @@ class hashbackup {
 		default => 'puppet:///modules/hashbackup/hb64',
 	}
 
-	file { 'hb':
-		path => '/usr/bin/hb',
+	file { $hashbackup:
 		ensure => file,
 		source => $binary,
 		owner => 'root',
 		group => 'root',
 		mode => 755
+	}
+
+	case $operatingsystem {
+			/(?i:RedHat|CentOS|Amazon|Fedora)/: {
+				file { "/etc/cron.daily/hashbackup.sh":
+					require => File[$hashbackup],
+					content => template('hashbackup/hashbackup.sh.erb'),
+					ensure => "file",
+					mode => 755,
+					owner => "root",
+					group => "root"
+				}
+			}
 	}
 }
